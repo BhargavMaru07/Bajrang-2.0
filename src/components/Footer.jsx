dec9422c49d3045ed1c247b284bc0114737a2e98
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2024-12-06 19:09:28 +0530

    changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
 const Footer = () => {
   return (
     <div className="bg-bg p-7 md:p-10">
-      <div className="container mx-auto my-0">
+      <div className="container mx-auto my-0 lg:max-w-[1330px]">
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 md:gap-2">
           <div className="my-2">
             <h2 className="mb-3 font-bold text-text">NEED HELP</h2>
@@ -56,7 +56,7 @@ const Footer = () => {
             <form className="flex flex-col">
               <input
                 type="email"
-                className="rounded border-none p-2 text-black ring-1 ring-btn ring-offset-2 placeholder:text-text focus:outline-none focus:ring"
+                className="p-2 text-black border-none rounded ring-1 ring-btn ring-offset-2 placeholder:text-text focus:outline-none focus:ring"
                 placeholder="user@xyz.com"
                 required
               />
@@ -69,7 +69,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="container flex flex-col justify-between p-4 sm:flex-row md:items-center">
-          <div className="my-3 flex flex-col items-center sm:flex-row md:m-0">
+          <div className="flex flex-col items-center my-3 sm:flex-row md:m-0">
             <div>
               <Link to="/">
                 <img src={logo} alt="logo" className="w-[150px]" />
