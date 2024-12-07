de64ade80da3315fa2652a8897bb768dd5663531
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2024-12-07 14:01:48 +0530

    complete banner section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import Slider from "react-slick";
-import slider_1 from "../assets/Banner/slide1.png";
+import slider_1 from "../assets/Banner/slide4.png";
 import slider_2 from "../assets/Banner/slide2.png";
 import slider_3 from "../assets/Banner/slide3.png";
 import Button from "../Styles/Button";
+import styled from "styled-components";
+import { slider } from "@material-tailwind/react";
 
 const HeroSection = () => {
   const SampleNextArrow = (props) => {
@@ -17,9 +19,13 @@ const HeroSection = () => {
           ...style,
           background: "transperent",
           width: "40px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           height: "40px",
-          border: "1px solid black",
+          // border: "1px solid black",
           borderRadius: "50%",
+          right: "0px",
         }}
         onClick={onClick}
       />
@@ -30,14 +36,17 @@ const HeroSection = () => {
     const { className, style, onClick } = props;
     return (
       <div
-        className={className}
+        className={`${className}`}
         style={{
-          ...style,
           background: "transperent",
           width: "40px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
           height: "40px",
-          border: "1px solid black",
+          // border: "1px solid black",
           borderRadius: "50%",
+          left: "0px",
         }}
         onClick={onClick}
       />
@@ -48,7 +57,7 @@ const HeroSection = () => {
     dots: true,
     infinite: true,
     speed: 600,
-    autoplay: true,
+    // autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplaySpeed: 3000,
@@ -58,54 +67,76 @@ const HeroSection = () => {
 
   const banner = [
     {
-      title: "Summer Sale",
-      description:
-        "Up to 50% off on selected items!\nDon't miss out on these deals.",
-      shop: "Shop Now",
+      title: "Elegant Latkans",
+      description: "Add charm to your lehenga with unique latkans.",
+      shop: "Browse Now",
       image: slider_1,
     },
     {
-      title: "New Arrivals",
-      description:
-        "Check out our latest collection.\nFresh styles for every season.",
-      shop: "Explore",
+      title: "Custom Latkans",
+      description: "Personalize your latkans for a perfect touch.",
+      shop: "Customize Now",
       image: slider_2,
     },
     {
-      title: "Exclusive Offers",
-      description: "Members get special discounts.\nJoin now and save big.",
-      shop: "Sign Up",
+      title: "Traditional Latkans",
+      description: "Explore intricate designs inspired by tradition.",
+      shop: "Explore Collection",
       image: slider_3,
     },
   ];
 
   return (
-    <div className="bg-[#F6F8FA] slider-container lg:h-[85vh] p-10  sm:h-[70vh]">
-      <Slider {...settings} className="h-[75vh]">
+    <Wrapper className="bg-[#F6F8FA] slider-container lg:h-[85vh] sm:p-3  md:h-[80vh] sm:h-[60vh] h-[98vh] px-2">
+      <Slider {...settings} className="md:h-[75vh] sm:h-[50vh] h-[90vh]">
         {banner.map((item, index) => {
           return (
-            <div key={index} className="container">
-              <div className="grid md:grid-cols-2 gap-2 h-[78vh]">
-                <div className="w-full p-5 px-12 m-auto">
-                  <div className="details">
-                    <h2 className="text-[#E2218F] text-5xl font-bold my-5">
+            <div key={index} className="container !flex m-auto">
+              <div className="grid md:grid-cols-3 gap-2 md:h-[80vh] sm:h-[55vh] sm:px-[50px]">
+                <div className="w-full m-auto md:pl-[0px] lg:px-16 order-last md:order-none sm:col-span-2">
+                  <div className="details lg:ml-[40px] md:ml-[0px] text-center sm:text-start">
+                    <h2 className="text-[#E2218F] sm:text-3xl font-bold my-5 text-[30px] lg:text-3xl">
                       {item.title}
                     </h2>
-                    <h1 className="my-3 text-5xl">{item.description}</h1>
+                    <h1 className="my-3 text-2xl sm:text-3xl lg:text-5xl !leading-[1.4]">
+                      {item.description}
+                    </h1>
                     <Button className="rounded-[23px]">{item.shop}</Button>
                   </div>
                 </div>
 
-                <div className="flex items-center justify-center">
-                  <img src={item.image} alt="banner" className="" />
+                <div className="flex items-center sm:justify-start h-[50vh] sm:h-auto sm:col-span-1 justify-center">
+                  <img
+                    src={item.image}
+                    alt="banner"
+                    className="h-full sm:h-auto"
+                  />
                 </div>
               </div>
             </div>
           );
         })}
       </Slider>
-    </div>
+    </Wrapper>
   );
 };
 
 export default HeroSection;
+
+const Wrapper = styled.section`
+  .slick-prev::before {
+    font-size: 46px !important;
+    height: 40px !important;
+    width: 46px !important;
+    opacity: 0.5;
+    color: black;
+  }
+
+  .slick-next::before {
+    font-size: 46px !important;
+    height: 40px !important;
+    width: 46px !important;
+    opacity: 0.5;
+    color: black;
+  }
+`;
