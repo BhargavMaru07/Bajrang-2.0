f1bfc344e2d24a8280959a13c936bae317ec5fa1
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2025-02-12 19:52:06 +0530

    try to solve error

--- Server/index.js
+++ Server/index.js
@@ -21,8 +21,9 @@ app.use(
   })
 );
 //middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded payload limit
+
 app.use(express.static(path.resolve("./public/uploads")));
 
 app.get("/", (req, res) => res.send("Hello This is Server !"));
--- Server/models/user-model.js
+++ Server/models/user-model.js
@@ -38,7 +38,6 @@ const UserSchema = new mongoose.Schema({
 
   profileImage: {
     type: String,
-    required: true,
   },
 
   password: {
